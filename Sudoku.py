9875f8b8ed100271d74086dd1d61efc9fe1e3fd2
edgarargueta <eaa7734@g.rit.edu>
2019-12-19 22:00:20 -0500

    Created the printBoard() function. It currently only prints out the contents of the sudoku board but I would like to make it look nicer in terms of a PTUI

--- Sudoku.py
+++ Sudoku.py
@@ -2,3 +2,15 @@
 Author: Edgar Argueta
 Filename: Sudoku.py
 """
+
+rows = 9
+cols = 9
+BOARD = [[0 for i in range(rows)] for j in range(cols)]
+
+def printBoard():
+    for x in range(rows):
+        for y in range(cols):
+            print(BOARD[x][y], end=' ')
+        print()
+
+printBoard()
