7eb0e558b4a7cefaf0262e9fdf6b7ff6d29cac2a
edgarargueta <eaa7734@g.rit.edu>
2019-12-27 00:00:55 -0500

    Created the check_square() to check the 3x3 square that no repeat of the number given is in it.

--- Sudoku.py
+++ Sudoku.py
@@ -6,12 +6,11 @@ import random
 
 ROWS = 9
 COLS = 9
-BOARD = [[0 for i in range (ROWS)] for j in range (COLS)]
-
+BOARD = [[0 for i in range(ROWS)] for j in range(COLS)]
 
 def print_board():
     print()
-    for x in range (ROWS):
+    for x in range(ROWS):
         print("\t", end='')
         for y in range(COLS):
             print(BOARD[x][y], end=' ')
@@ -34,19 +33,27 @@ def check_col(num, row, col):
     return True
 
 
+def check_square(num, row, col):
+    start_x = row - (row % 3)
+    start_y = col - (col % 3)
+    for x in range(start_x,start_x+3):
+        for y in range(start_y, start_y+3):
+            if x != row and y != col:
+                if BOARD[x][y] == num:
+                    return False
+    return True
+
+
 def main():
     BOARD[1][5] = 5
     BOARD[3][5] = 5
     BOARD[0][2] = 3
     BOARD[0][7] = 5
-    if check_col(5, 0, 5):
-        print("FAILURE #1")
-    if check_col(3, 0, 2):
-        print("SUCCESS #1")
-    if check_row(5, 1, 5):
-        print("FAILURE #2")
-    if check_row(3, 0, 2):
-        print("SUCCESS #2")
+    BOARD[2][0] = 3
+    if check_square(3, 0, 2):
+        print("FAILURE")
+    if check_square(5, 0, 7):
+        print("SUCCESS")
     print_board()
 
 
